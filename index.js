68784db3883bbcf18e8e9f49854aac8ac064c50b
Artur <trucule@gmail.com>
2023-01-18 15:05:24 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+const otherStuff = "Hello. This is other stuff."
+
+//Set Handlebar routes
+app.get('/', (req, res) => {
+    res.render('home', {
+    	stuff: otherStuff
+    });
+});
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
+
